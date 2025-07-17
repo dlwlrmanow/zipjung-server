a1920a50cd0683f7f304e64c245ac2f1d6959279
dlwlrmanow <lullulalla9711@gmail.com>
2025-07-17 20:55:03 +0900

    edit: 사용자 집중 시간 중 아직 기록이 없는 집중시간만 리스트 뽑기

--- src/main/java/com/zipjung/backend/repository/FocusTimeRepository.java
+++ src/main/java/com/zipjung/backend/repository/FocusTimeRepository.java
@@ -14,6 +14,7 @@ public interface FocusTimeRepository extends JpaRepository<FocusTime, Long> {
     @Query("UPDATE FocusTime f SET f.focusLogId = :focusLogId WHERE f.id = :focusTimeId")
     int updateFocusLogId(@Param("focusLogId") Long focusLogId, @Param("focusTimeId")Long focusTimeId);
 
-    @Query("SELECT f FROM FocusTime f WHERE f.createdAt >= :oneWeekAgo AND f.isDeleted = false ") // 최근 일주일내 기록만 가져오기
+    // TODO: 사용자 집중 시간 리스트에서 최근 일주일 집중시간 + 시간에 대한 기록 없는 것만!!
+    @Query("SELECT f FROM FocusTime f WHERE f.createdAt >= :oneWeekAgo AND f.isDeleted = false AND f.focusLogId IS NULL") // 최근 일주일내 기록만 가져오기
     List<FocusTime> getRecentWeekFocusTimes(@Param("oneWeekAgo") LocalDateTime oneWeekAgo);
 }
